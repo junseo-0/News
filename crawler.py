89e0589fa0046c596f6c048f74be599b291fb551
junseo-0 <sbpark@salin.co.kr>
2024-07-02 11:17:43 +0900

    검색기사정보로 수정

--- crawler.py
+++ crawler.py
@@ -21,27 +21,27 @@ def crawl_news(keyword, num_news, max_retries=3):
     
     try:
         # 뉴스 섹션이 로드될 때까지 대기
-        news_section_xpath = '//*[@id="fullColumn"]/div/div[4]/div[3]'
+        news_section_selector = "#fullColumn > div > div:nth-child(6) > div.searchSectionMain > div"
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, news_section_xpath))
+            EC.presence_of_element_located((By.CSS_SELECTOR, news_section_selector))
         )
 
         while len(news_items) < num_news:
             # 뉴스 섹션 내에서 기사 찾기
-            news_section = driver.find_element(By.XPATH, news_section_xpath)
-            articles = news_section.find_elements(By.XPATH, './/article')
+            news_section = driver.find_element(By.CSS_SELECTOR, news_section_selector)
+            articles = news_section.find_elements(By.CSS_SELECTOR, 'div > div > a')
             
             for article in articles:
                 if len(news_items) >= num_news:
                     break
                 
                 try:
-                    title_element = article.find_element(By.XPATH, './/a[@class="title"]')
-                    title = title_element.text.strip()
-                    link = title_element.get_attribute('href')
+                    title = article.text.strip()
+                    link = article.get_attribute('href')
                     if {'title': title, 'link': link} not in news_items:
                         news_items.append({'title': title, 'link': link})
-                except NoSuchElementException:
+                except Exception as e:
+                    print(f"Error extracting article info: {str(e)}")
                     continue
             
             # 페이지 끝까지 스크롤
@@ -49,7 +49,7 @@ def crawl_news(keyword, num_news, max_retries=3):
             time.sleep(2)  # 새로운 내용이 로드될 때까지 대기
             
             # 새로운 기사가 로드되지 않으면 종료
-            new_articles = news_section.find_elements(By.XPATH, './/article')
+            new_articles = news_section.find_elements(By.CSS_SELECTOR, 'div > div > a')
             if len(new_articles) == len(articles):
                 break
 
