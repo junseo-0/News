3a3b891c9e43f14ed6f75123ff07cf85355a205f
junseo-0 <sbpark@salin.co.kr>
2024-07-02 11:13:56 +0900

    다시

--- crawler.py
+++ crawler.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
@@ -21,23 +20,23 @@ def crawl_news(keyword, num_news, max_retries=3):
     news_items = []
     
     try:
-        # 특정 섹션이 로드될 때까지 대기
-        section_selector = "#fullColumn > div > div:nth-child(6) > div.searchSectionMain > div"
+        # 뉴스 섹션이 로드될 때까지 대기
+        news_section_xpath = '//*[@id="fullColumn"]/div/div[4]/div[3]'
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, section_selector))
+            EC.presence_of_element_located((By.XPATH, news_section_xpath))
         )
 
         while len(news_items) < num_news:
-            # 특정 섹션 내에서 기사 찾기
-            section = driver.find_element(By.CSS_SELECTOR, section_selector)
-            articles = section.find_elements(By.CSS_SELECTOR, '.js-article-item')
+            # 뉴스 섹션 내에서 기사 찾기
+            news_section = driver.find_element(By.XPATH, news_section_xpath)
+            articles = news_section.find_elements(By.XPATH, './/article')
             
             for article in articles:
                 if len(news_items) >= num_news:
                     break
                 
                 try:
-                    title_element = article.find_element(By.CSS_SELECTOR, 'a.title')
+                    title_element = article.find_element(By.XPATH, './/a[@class="title"]')
                     title = title_element.text.strip()
                     link = title_element.get_attribute('href')
                     if {'title': title, 'link': link} not in news_items:
@@ -50,7 +49,7 @@ def crawl_news(keyword, num_news, max_retries=3):
             time.sleep(2)  # 새로운 내용이 로드될 때까지 대기
             
             # 새로운 기사가 로드되지 않으면 종료
-            new_articles = section.find_elements(By.CSS_SELECTOR, '.js-article-item')
+            new_articles = news_section.find_elements(By.XPATH, './/article')
             if len(new_articles) == len(articles):
                 break
 
